82bd08ad9dcbd29608cc45da6a95874c9cac384b
pisekpa <kong39615@gmail.com>
2019-10-24 13:59:51 +0700

    (2)Drop - Down Lists - Write Some Code

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
@@ -7,9 +7,21 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	private String country;
+	
+	private LinkedHashMap<String , String> countryOptions;
 		
 	public Student() {
 		
+		// populate country options: used ISO country code
+		countryOptions = new LinkedHashMap<>();
+		
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+		countryOptions.put("US", "United States of America");
+		
+
 	}
 
 	public String getFirstName() {
@@ -34,6 +46,10 @@ public class Student {
 
 	public void setCountry(String country) {
 		this.country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
 	}
 	
 	
