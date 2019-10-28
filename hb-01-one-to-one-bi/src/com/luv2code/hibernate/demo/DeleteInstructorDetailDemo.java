58954b1c5fd2acc5ef77d440b6d89464dba783f3
pisekpa <kong39615@gmail.com>
2019-10-28 14:33:45 +0700

    @OneToOne - Bi-Directional - Delete Only InstructorDetail

--- hb-01-one-to-one-bi/src/com/luv2code/hibernate/demo/DeleteInstructorDetailDemo.java
+++ hb-01-one-to-one-bi/src/com/luv2code/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -28,7 +28,7 @@ public class DeleteInstructorDetailDemo {
 			session.beginTransaction();
 
 			// get the instructor detail object
-			int theId = 2;
+			int theId = 3;
 			InstructorDetail tempInstructorDetail = 
 					session.get(InstructorDetail.class, theId);
 			
@@ -42,6 +42,11 @@ public class DeleteInstructorDetailDemo {
 			// now let's delete the instructor detail
 			System.out.println("Deleting tempInstructorDetail: " 
 											+ tempInstructorDetail);
+			
+			//remove the associated object reference
+			//break bi-directional link			
+			tempInstructorDetail.getInstructor().setInstructorDetail(null);
+			
 
 			session.delete(tempInstructorDetail);
 			
