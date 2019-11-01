204c0f2fa6e9a46d49ae539b2429ee7bf411efd6
pisekpa <kong39615@gmail.com>
2019-11-01 16:49:15 +0700

    Add Customer -Save Database

--- web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -27,13 +27,26 @@ public class CustomerDAOImpl implements CustomerDAO {
 				
 		// create a query
 		Query<Customer> theQuery = 
-				currentSession.createQuery("from Customer", Customer.class);
+				currentSession.createQuery("from Customer order by lastName"
+											, Customer.class);
 		
 		// execute query and get result list
 		List<Customer> customers = theQuery.getResultList();
 				
 		// return the results		
 		return customers;
+	}
+
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// save the customer ... finally LOL
+		currentSession.save(theCustomer);
+		
 	}
 
 }
