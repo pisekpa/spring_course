2f3a6c1a96b65b30de2cf3090158f4261b3ac1ff
pisekpa <kong39615@gmail.com>
2020-02-22 08:15:39 +0700

    547 - 548 : Spring Boot - Thymeleaf - CRUD Database Project - Get Employee

--- 34-thymeleafdemo-employees-list-db/src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
+++ 34-thymeleafdemo-employees-list-db/src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
@@ -1,16 +1,39 @@
-package com.luv2code.springboot.thymeleafdemo.model;
+package com.luv2code.springboot.thymeleafdemo.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
 public class Employee {
 
+	// define fields
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id;
+	
+	@Column(name="first_name")
 	private String firstName;
+	
+	@Column(name="last_name")
 	private String lastName;
+	
+	@Column(name="email")
 	private String email;
-
+	
+		
+	// define constructors
+	
 	public Employee() {
 		
 	}
-
+	
 	public Employee(int id, String firstName, String lastName, String email) {
 		this.id = id;
 		this.firstName = firstName;
@@ -18,6 +41,15 @@ public class Employee {
 		this.email = email;
 	}
 
+
+	public Employee(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	// define getter/setter
+	
 	public int getId() {
 		return id;
 	}
@@ -50,12 +82,22 @@ public class Employee {
 		this.email = email;
 	}
 
+	// define tostring
+
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
 	}
 		
 }
+
+
+
+
+
+
+
+
 
 
 
