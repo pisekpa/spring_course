6838d199d1823caeee15c0e7921b1bf445f8d293
pisekpa <kong39615@gmail.com>
2020-02-22 09:13:36 +0700

    552 - 553 : Spring Boot - Thymeleaf - CRUD Database Real - Time Project: Update Employee

--- 36-thymeleafdemo-employees-update/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 36-thymeleafdemo-employees-update/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
@@ -46,6 +47,20 @@ public class EmployeeController {
 		theModel.addAttribute("employee", theEmployee);
 		
 		return "employees/employee-form";
+	}
+	
+	@PostMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId,
+									Model theModel) {
+		
+		// get the employee from the service
+		Employee theEmployee = employeeService.findById(theId);
+		
+		// set employee as a model attribute to pre-populate the form
+		theModel.addAttribute("employee", theEmployee);
+		
+		// send over to our form
+		return "employees/employee-form";			
 	}
 	
 	@PostMapping("/save")
